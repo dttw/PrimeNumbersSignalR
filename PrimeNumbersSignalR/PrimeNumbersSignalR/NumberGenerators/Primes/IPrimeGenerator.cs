769284e86f07e382ab1ae7bc53af1bf75d402792
Graham Whittenham <whittenhamg@gmail.com>
2018-06-12 21:31:24 +0100

    Added prime generator interface and class.

---
+++ PrimeNumbersSignalR/PrimeNumbersSignalR/NumberGenerators/Primes/IPrimeGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumbersSignalR.NumberGenerators.Primes
+{
+    public interface IPrimeGenerator
+    {
+        IList<int> FindPrimesUpTo(int primeLimit);
+
+        IList<int> FindAmountOfPrimes(int amountOfPrimes);
+    }
+}
---
+++ PrimeNumbersSignalR/PrimeNumbersSignalR/NumberGenerators/Primes/SieveOfAtkinPrimeGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PrimeNumbersSignalR.NumberGenerators.Primes
+{
+    public class SieveOfAtkinPrimeGenerator : IPrimeGenerator
+    {
+        public IList<int> FindAmountOfPrimes(int amountOfPrimes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<int> FindPrimesUpTo(int primeLimit)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
