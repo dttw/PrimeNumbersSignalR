06c61d9121aeef51657cbe63083e0888ab8b1cfc
Graham Whittenham <whittenhamg@gmail.com>
2018-06-12 19:05:22 +0100

    Added SignalR v2.2.3.

---
+++ PrimeNumbersSignalR/PrimeNumbersSignalR.Tests/Hubs/NumberGeneratorHubTests.cs
@@ -0,0 +1,16 @@
+﻿using PrimeNumbersSignalR.Hubs;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PrimeNumbersSignalR.Hubs.Tests
+{
+    [TestClass()]
+    public class NumberGeneratorHubTests
+    {
+        [TestMethod()]
+        public void GeneratePrimesTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
