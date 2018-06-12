649a27288e009b4ece2f9ad70644130279d52bb8
Graham Whittenham <whittenhamg@gmail.com>
2018-06-12 19:48:45 +0100

    Added startup class and mapped signalr.

---
+++ PrimeNumbersSignalR/PrimeNumbersSignalR/App_Start/Startup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin;
+using Owin;
+using PrimeNumbersSignalR.Hubs;
+
+[assembly: OwinStartup(typeof(PrimeNumbersSignalR.App_Start.Startup))]
+
+namespace PrimeNumbersSignalR.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+            app.MapSignalR();
+        }
+    }
+}
