1c7e845157088b1e74fae1850d658053b52b77c8
Graham Whittenham <whittenhamg@gmail.com>
2018-06-14 13:45:20 +0100

    Added class to hold shared methods for prime sieves. Created method which approximates the limit for a sieve based on the amount of primes requested.

---
+++ PrimeNumbersSignalR/PrimeNumbersSignalR/NumberGenerators/Primes/Sieves/PrimeSieves.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PrimeNumbersSignalR.NumberGenerators.Primes.Sieves
+{
+    public static class PrimeSieves
+    {
+        public static long ApproximatePrimeLimit(int amountOfPrimes)
+        {
+            double n = (double)amountOfPrimes;
+            double p = 0;
+
+            if (amountOfPrimes >= 7022)
+            {
+                p = n * Math.Log(n) + n * (Math.Log(Math.Log(n)) - 0.9385);
+            }
+            else if (amountOfPrimes >= 6)
+            {
+                p = n * Math.Log(n) + n * Math.Log(Math.Log(n));
+            }
+            else if (amountOfPrimes > 0)
+            {
+                p = new int[] { 2, 3, 5, 7, 11 }[amountOfPrimes - 1];
+            }
+
+            return (long)p;
+        }
+    }
+}
