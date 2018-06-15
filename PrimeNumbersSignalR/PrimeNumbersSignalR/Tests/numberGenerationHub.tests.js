1d6cd4813b0aecdbb904c11cc73506d2e01c1871
Graham Whittenham <whittenhamg@gmail.com>
2018-06-15 16:02:14 +0100

    Tests for createNumberGenerationHub.

--- PrimeNumbersSignalR/PrimeNumbersSignalR/Tests/numberGenerationHub.tests.js
+++ PrimeNumbersSignalR/PrimeNumbersSignalR/Tests/numberGenerationHub.tests.js
@@ -12,12 +12,44 @@ describe("Create a number generation hub.",
                 expect(message).toBe("You must specify a message Div.");
             });
     }
-);
+    ,
+    // Test createNumberGenerationHub with empty string messageDiv
+    function () {
+        it("Should return message that a message Div is required",
 
-// Test createNumberGenerationHub with empty string messageDiv
-// Test createNumberGenerationHub with wrong parameter type
-// Test createNumberGenerationHub with valid messageDiv
-// Test created hub has maxPrimeLimit, primesRequestedOutOfRange and generatedNumbers methods
+            function () {
+                var message = createNumberGenerationHub("");
+
+                expect(message).toBe("You must specify a message Div.");
+            });
+    }
+    ,
+    // Test createNumberGenerationHub with wrong parameter type
+    function () {
+        it("Should return message that a message Div must be a string",
+
+            function () {
+                var message = createNumberGenerationHub(1);
+
+                expect(message).toBe("Message div must be a valid string.");
+            });
+    }
+    ,
+    // Test createNumberGenerationHub with valid messageDiv
+    // has maxPrimeLimit, primesRequestedOutOfRange and generatedNumbers methods
+    function () {
+        it("Should return a hub with maxPrimeLimit, primesRequestedOutOfRange and generatedNumbers client methods.",
+
+            function () {
+                var generationHub = createNumberGenerationHub("messages");
+
+                expect(generationHub.client.maxPrimeLimit).not(typeof 'undefined').not(null);
+                expect(generationHub.client.primesRequestedOutOfRange).not(typeof 'undefined').not(null);
+                expect(generationHub.client.generatedNumbers).not(typeof 'undefined').not(null);
+                expect(generationHub.server.maxPrimeLimit).not(typeof 'undefined').not(null);
+                expect(hub.server.generateAmountOfPrimes).not(typeof 'undefined').not(null);
+            }
+        );
 
 // Test connectToNumberGenerationHub with null hub
 // Test connectToNumberGenerationHub with wrong parameter type
