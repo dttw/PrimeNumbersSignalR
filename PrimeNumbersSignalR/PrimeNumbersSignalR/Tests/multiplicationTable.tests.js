3edff003e1832b77d31be78e407b311b26075174
Graham Whittenham <whittenhamg@gmail.com>
2018-06-15 19:18:43 +0100

    Fixed createMultiplicationTableCell valid test to check if returned element is a <td> and has a value of 2.

--- PrimeNumbersSignalR/PrimeNumbersSignalR/Tests/multiplicationTable.tests.js
+++ PrimeNumbersSignalR/PrimeNumbersSignalR/Tests/multiplicationTable.tests.js
@@ -230,7 +230,7 @@ describe('Tests for createMultiplicationTableHeaderCell.',
     // Test createMultiplicationTableHeaderCell with valid multiplicand
     describe('Try to create header row with array of ints for table numbers.',
         function () {
-            it('Should return an html table cell',
+            it('Should return an html table header cell',
 
                 function () {
                     var header = createMultiplicationTableHeaderCell(1);
@@ -482,12 +482,14 @@ describe('Tests for createMultiplicationTableCell.',
     // Test createMultiplicationTableCell with valid multiplier & multiplicand
     describe('Try to create a table cell with valid multiplier and multiplicand.',
         function () {
-            it('Should return return an html table cell',
+            it('Should return return an html table cell with the correct multiplied value.',
 
                 function () {
-                    var message = createMultiplicationTableCell(1, 2);
+                    var generatedTableCell = createMultiplicationTableCell(1, 2);
 
-                    expect(header.html()).toContain('<td>');
+                    expect(generatedTableCell.is('td')).toBeTruthy();
+
+                    expect(generatedTableCell.html).toContain('2');
                 });
         }
     )
