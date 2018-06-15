b91b42eb963b706048a43ff2395b7b2cfa4807b9
Graham Whittenham <whittenhamg@gmail.com>
2018-06-15 15:52:04 +0100

    Added test for sending null message div to createNumberGenerationHub.

--- PrimeNumbersSignalR/PrimeNumbersSignalR/Tests/numberGenerationHub.tests.js
+++ PrimeNumbersSignalR/PrimeNumbersSignalR/Tests/numberGenerationHub.tests.js
@@ -1,4 +1,19 @@
-﻿// Test createNumberGenerationHub with null messageDiv
+﻿/// <reference path="../Scripts/jasmine/jasmine.js" />
+/// <reference path="../Source/numberGenerationHub.js" />
+
+describe("Create a number generation hub.",
+    // Test createNumberGenerationHub with null messageDiv
+    function () {
+        it("Should return message that a message Div is required",
+
+            function () {
+                var message = createNumberGenerationHub(null);
+
+                expect(message).toBe("You must specify a message Div.");
+            });
+    }
+);
+
 // Test createNumberGenerationHub with empty string messageDiv
 // Test createNumberGenerationHub with wrong parameter type
 // Test createNumberGenerationHub with valid messageDiv
