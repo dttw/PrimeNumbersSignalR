47827c9526e89b1494e43c8c08f0c6612a20ea6c
Graham Whittenham <whittenhamg@gmail.com>
2018-06-15 12:25:09 +0100

    Added mock prime number generator class.

---
+++ PrimeNumbersSignalR/PrimeNumbersSignalR.Tests/NumberGenerators/Primes/MockPrimeGenerator.cs
@@ -0,0 +1,31 @@
+﻿using PrimeNumbersSignalR.NumberGenerators.Primes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumbersSignalR.Tests.NumberGenerators.Primes
+{
+    public class MockPrimeGenerator : IPrimeGenerator
+    {
+        private int maxPrimeLimit = 10;
+        private IList<long> primesFound = new List<long>();
+        public int MaxPrimeLimit => throw new NotImplementedException();
+
+        public IList<long> FindAmountOfPrimes(int amountOfPrimes)
+        {
+            MockGeneration(amountOfPrimes);
+
+            return primesFound;
+        }
+
+        private void MockGeneration(int amountOfPrimes)
+        {
+            for (long fakePrime = 0; fakePrime < amountOfPrimes; fakePrime++)
+            {
+                primesFound.Add(fakePrime);
+            }
+        }
+    }
+}
