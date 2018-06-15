3e81298282f7f70155bc93b79e483d3da5fa0635
Graham Whittenham <whittenhamg@gmail.com>
2018-06-15 14:25:30 +0100

    Added notes for required tests.

---
+++ PrimeNumbersSignalR/PrimeNumbersSignalR/Tests/multiplicationTable.tests.js
@@ -0,0 +1,48 @@
+﻿// Test createMultiplicationTable with null table numbers
+// Test createMultiplicationTable with non int table numbers
+// Test createMultiplicationTable with null table class
+// Test createMultiplicationTable with non string table class
+// Test createMultiplicationTable with valid table numbers & table class
+// Test that createMultiplicationTable returns a valid table
+
+
+// Test createMultiplicationTableHeader with null table numbers
+// Test createMultiplicationTableHeader with non int table numbers
+// Test createMultiplicationTableHeader with valid table numbers
+// Test that createMultiplicationTableHeader returns a valid table header
+
+
+// Test createMultiplicationTableHeaderRow with null table numbers
+// Test createMultiplicationTableHeaderRow with non int table numbers
+// Test createMultiplicationTableHeaderRow with valid table numbers
+// Test that createMultiplicationTableHeaderRow returns a valid table header row
+
+
+// Test createMultiplicationTableHeaderCell with null multiplicand
+// Test createMultiplicationTableHeaderCell with non int multiplicand
+// Test createMultiplicationTableHeaderCell with valid multiplicand
+// Test that createMultiplicationTableHeaderCell returns a valid table header cell
+
+
+// Test createMultiplicationTableBody with null tableNumbers
+// Test createMultiplicationTableBody with non int tableNumbers
+// Test createMultiplicationTableBody with valid tableNumbers
+// Test that createMultiplicationTableBody returns a valid table body
+
+
+// Test createMultiplicationTableRow with null multiplier
+// Test createMultiplicationTableRow with non int multiplier
+// Test createMultiplicationTableRow with valid multiplier
+// Test createMultiplicationTableRow with non int tableNumbers
+// Test createMultiplicationTableRow with valid tableNumbers
+// Test createMultiplicationTableRow with null tableNumbers & tableNumbers
+// Test that createMultiplicationTableRow returns a valid table row
+
+
+// Test createMultiplicationTableCell with null multiplier
+// Test createMultiplicationTableCell with non int multiplier
+// Test createMultiplicationTableCell with valid multiplier
+// Test createMultiplicationTableCell with null multiplicand
+// Test createMultiplicationTableCell with non int multiplicand
+// Test createMultiplicationTableCell with valid multiplier & multiplicand
+// Test that createMultiplicationTableCell returns a valid table cell
---
+++ PrimeNumbersSignalR/PrimeNumbersSignalR/Tests/numberGenerationHub.tests.js
@@ -0,0 +1,21 @@
+﻿// Test createNumberGenerationHub with null messageDiv
+// Test createNumberGenerationHub with empty string messageDiv
+// Test createNumberGenerationHub with wrong parameter type
+// Test createNumberGenerationHub with valid messageDiv
+// Test created hub has maxPrimeLimit, primesRequestedOutOfRange and generatedNumbers methods
+
+// Test connectToNumberGenerationHub with null hub
+// Test connectToNumberGenerationHub with wrong parameter type
+// Test connectToNumberGenerationHub with valid hub
+
+// Test generatePrimeMultiplicationTable with null amountOfPrimes
+// Test generatePrimeMultiplicationTable with non int amountOfPrimes
+// Test generatePrimeMultiplicationTable with int amountOfPrimes
+// Test generatePrimeMultiplicationTable with null generationHub
+// Test generatePrimeMultiplicationTable with wrong type for generationHub
+// Test generatePrimeMultiplicationTable with valid generationHub & amountOfPrimes
+
+// Test displayMessage with null messageDiv
+// Test displayMessage with null messageText
+// Test displayMessage with valid messageDiv
+// Test displayMessage with valid messageText
