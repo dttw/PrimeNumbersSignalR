755aab2403d598f84f701e53bb4b43dbeacc1038
Graham Whittenham <whittenhamg@gmail.com>
2018-06-15 13:54:50 +0100

    Added empty methods with comments describing functionality.

--- PrimeNumbersSignalR/PrimeNumbersSignalR/Source/hubs/numberGenerationHub.js
+++ PrimeNumbersSignalR/PrimeNumbersSignalR/Source/hubs/numberGenerationHub.js
@@ -1,4 +1,15 @@
-﻿
-// TODO: setup hub connection
-// TODO: make call to generate primes
-// TODO: recieve numbers and build multiplication table
+﻿function createNumberGenerationHub(messageDiv) {
+    // TODO: Create and return the hub connection
+}
+
+function connectToNumberGenerationHub(generationHub) {
+    // TODO: Start the hub connection and run any functions required after the connection starts
+}
+
+function generatePrimeMultiplicationTable(amountOfPrimes, hub) {
+    // TODO: Send request to hub for amount of primes specified
+}
+
+function displayMessage(messageDiv, messageText) {
+    // TODO: Write a message to a div. Probably want this somewhere more general eventually.
+}
--- PrimeNumbersSignalR/PrimeNumbersSignalR/Source/userinterface/multiplicationtable.js
+++ PrimeNumbersSignalR/PrimeNumbersSignalR/Source/userinterface/multiplicationtable.js
@@ -1,3 +1,41 @@
-﻿
-// TODO: function to accept an array of numbers
+﻿// TODO: function to accept an array of positive ints
 // TODO: function to write the table from numbers supplied
+// TODO: Display numbers in max of 50 * 50 chunks
+// TODO: Page around table horizontally and vertically in 50 * 50 chunks
+
+function createMultiplicationTable(tableNumbers, tableClass) {
+    // TODO: Create table
+    // TODO: Create header and add to table
+    // TODO: Create body and add to table
+    // TODO: Return table
+}
+
+function createMultiplicationTableHeader(tableNumbers) {
+    // TODO: Create table header
+    // TODO: Return table header
+}
+
+function createMultiplicationTableHeaderRow(tableNumbers) {
+    // TODO: Create header row
+    // TODO: Return header row
+}
+
+function createMultiplicationTableHeaderCell(multiplicand) {
+    // TODO: Create header cell
+    // TODO: Return header cell
+}
+
+function createMultiplicationTableBody(tableNumbers) {
+    // TODO: Create table body
+    // TODO: Return table body
+}
+
+function createMultiplicationTableRow(multiplier, tableNumbers) {
+    // TODO: Create table body
+    // TODO: return table body
+}
+
+function createMultiplicationTableCell(multiplier, multiplicand) {
+    // TODO: Create table cell
+    // TODO: return table cell
+}
